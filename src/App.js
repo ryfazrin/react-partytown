3054791a18ba1a956fd2a4086af2b95b90a9a262
ryfazrin <riansera515@gmail.com>
2022-04-08 23:10:01 +0800

    added dummy Third-Party Script

--- src/App.js
+++ src/App.js
@@ -7,6 +7,27 @@ function App() {
     <div className="App">
       <Head />
       <header className="App-header">
+        
+      <strong>window.fetch(url) text</strong>
+      <code>
+        <span id="testFetchText"></span>
+      </code>
+      
+      <script
+        type="text/partytown"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (async function () {
+              const elm = document.getElementById('testFetchText');
+              const rsp = await window.fetch('text.txt');
+              const text = await rsp.text();
+              elm.textContent = text;
+              elm.className = 'testFetchText';
+            })();
+          `,
+        }}
+      />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
